40aaa565f71f336ff181c531a2689c62cc95a29c
Arjun Bhusal <rhythemechristthelion@gmail.com>
2023-04-04 08:07:52 +0545

    Quiz App

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Who is the CEO of Google?",
+            "Which one is not the programming language?",
+            "Who is father of Economics?",
+            "Who is founder of FaceBook?"
+    };
+
+    public static String choices[][] = {
+            {"Larry page","Sergey Brin","Martin Cooper","Both A & B"},
+            {"JAVA","Python","C++","Adobe PhotoShop"},
+            {"Adam Smith","Alfred Marshall","Robinson","David Richardo"},
+            {"Mark Zukerberg","Jeff Bezoz","Mukesh Ambani","Binod Chaudhary"}
+
+    };
+    public static String correctAnswers[] ={
+            "Both A & B",
+            "Adobe PhotoShop",
+            "Adam Smith",
+            "Mark Zukerberg"
+
+    };
+}
